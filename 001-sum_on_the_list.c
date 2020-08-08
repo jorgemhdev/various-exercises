9d40388b559328c3ac31e662f9374cf194041d22
Jorge Martínez Hurtado <45176388+jorgemhdev@users.noreply.github.com>
2020-08-08 19:38:26 +0200

    file: updated 001-sum_on_the_list.c
    Fixed description on the .c file.

--- 001-sum_on_the_list.c
+++ 001-sum_on_the_list.c
@@ -1,10 +1,15 @@
 /*
 
+Exercise:
+
 Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
-
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
-
 Bonus: Can you do this in one pass?
+
+-------------------------------------------------------
+
+This code can be shared or reused as long as the original author is credited:
+Jorge Martinez Hurtado | jorgemhdev (2020)
 
 */
 
